4bf7e3a7c28c59527d8d56ff3bd0288d5dfc3ea2
Abe <abhi.rawat456@gmail.com>
2020-06-27 17:02:15 +0530

    Made UI for each video

---
+++ My Youtube App/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  My Youtube App
+//
+//  Created by Abhishek Rawat on 27/06/20.
+//  Copyright © 2020 Abhishek Rawat. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
