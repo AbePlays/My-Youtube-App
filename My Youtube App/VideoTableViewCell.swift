7c7ed1788bad0bdb046fcde2d077075e4dbf5733
Abe <abhi.rawat456@gmail.com>
2020-06-27 17:17:44 +0530

    Coded the video cell

--- My Youtube App/VideoTableViewCell.swift
+++ My Youtube App/VideoTableViewCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class VideoTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var video:Video?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +25,38 @@ class VideoTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setCell(_ v:Video) {
+        self.video = v
+        
+        guard self.video != nil else {
+            return
+        }
+        
+        self.titleLabel.text = video?.title
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        self.dateLabel.text = df.string(from: video!.published)
+        
+        guard self.video!.thumbnail != nil else {
+            return
+        }
+        
+        let url = URL(string: self.video!.thumbnail)
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            if error == nil && data != nil {
+                if url?.absoluteString != self.video?.thumbnail {
+                    return
+                }
+                let image = UIImage(data: data!)
+                DispatchQueue.main.async {
+                    self.thumbnailImageView.image = image
+                }
+            }
+        }
+        dataTask.resume()
     }
 
 }
